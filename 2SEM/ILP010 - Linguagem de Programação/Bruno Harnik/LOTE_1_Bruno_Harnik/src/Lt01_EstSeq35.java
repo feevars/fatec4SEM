19cd8dc79b5dd3e07a4e4c72419ca0f194d03244
Bruno Harnik <brunoharnik@gmail.com>
2019-02-22 01:39:48 -0300

    35 satoshera

--- 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/LOTE_1_Bruno_Harnik/src/Lt01_EstSeq35.java
+++ 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/LOTE_1_Bruno_Harnik/src/Lt01_EstSeq35.java
@@ -1,6 +1,3 @@
-
-import javax.swing.JOptionPane;
-
 /*
  *************************
 Lote 01 - Exercício 35
@@ -12,16 +9,52 @@ Professor: Ricardo Satoshi
  *************************
  */
 
+import javax.swing.JOptionPane;
+
 public class Lt01_EstSeq35 {
+    
     public static void main(String args[]){
         
-        int valor1, valor2;
+        String resultado = "";
         
-        valor1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um valor inteiro: "));
-        valor2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite outro valor inteiro: "));
+        int v1, v2, aux, soma_impar = 0;
         
-        if ()
         
+        
+        v1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um valor inteiro: "));
+        v2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite outro valor inteiro: "));
+        
+        if(v1 == v2){
+            
+            resultado += "Os valores inseridos são iguais... \n\n";
+            
+        }else if(v2 > v1){
+            
+            resultado += "O maior valor inserido foi " + v2 + ".\n\n";
+            
+        }else{
+            
+            resultado += "O maior valor inserido foi " + v1 + ".\n\n";
+            
+            aux = v1;
+            v1 = v2;
+            v2 = aux;
+            
+        }
+        
+        for(aux = v1 + 1; aux < v2; aux++){
+            
+            if(aux % 2 != 0){
+                
+                soma_impar = soma_impar + aux;
+                
+            }
+            
+        }
+        
+        resultado += "A soma dos números ímpares entre " + v1 + " e " + v2 + " é " + soma_impar + ".";
+        
+        JOptionPane.showMessageDialog(null, resultado);
         
     }
 }
