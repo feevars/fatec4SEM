3fdf253073987b84fb4a1a108c9a9c938858afe5
Bruno Harnik <brunoharnik@gmail.com>
2019-03-25 01:09:03 -0300

    novos de vetores

--- 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/Prj_Recursividade_Bruno_Harnik/src/RecSeq01.java
+++ 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/Prj_Recursividade_Bruno_Harnik/src/RecSeq01.java
@@ -1,13 +1,36 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+***********************************************
 
-/**
- *
- * @author bruno
- */
+    1)   Serie1 = (1+2+3+...+100)
+
+    Programador: Bruno Bega Harnik
+    Professor: Ricardo Satoshi
+
+***********************************************
+*/
+
+import javax.swing.JOptionPane;
+
 public class RecSeq01 {
+    
+    public static void main(String[] args){
+        
+        JOptionPane.showMessageDialog(null, Soma1(1,0));
+        
+    }
+    
+    static int Soma1(int i, int Soma) { 
+        
+        if (i == 100) {
+            
+            Soma += i;
+            
+            return Soma;  
+            
+        } else {
+           
+           return Soma1(i + 1, Soma + i); 
+        }
+    }
     
 }
