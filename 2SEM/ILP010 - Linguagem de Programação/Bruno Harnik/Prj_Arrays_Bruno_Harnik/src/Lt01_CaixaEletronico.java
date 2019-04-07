0f8bb1416570084da3fe360bf1862bfd8f038f3f
Bruno Harnik <brunoharnik@gmail.com>
2019-04-07 16:11:50 -0300

    Enunciado do caixa eletrônico

---
+++ 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/Prj_Arrays_Bruno_Harnik/src/Lt01_CaixaEletronico.java
@@ -0,0 +1,71 @@
+/*
+ * Caixa Eletrônico
+ * 
+ * 1.   Criar um menu de opções:
+ *
+ *      Menu Principal
+ *      1 - Carregar Notas
+ *      2 - Retirar Notas
+ *      3 - Estatística
+ *      9 - Fim
+ *
+ *
+ * 2.   Carregar a quantidade de notas em uma
+ *      área da memória com 6 ocorrências.
+ *
+ *
+ * 3.   Solicitar que o cliente faça a retirada de valores
+ *      obedecendo ao critério do maior pelo menor.
+ *
+ *
+ * 4.   Dar a opção para o cliente escolher o valor e a quantidade de notas.
+ *      P. ex.: 1 x 20, 2 x 10...
+ *
+ *
+ * 5.   Caso não tenha o valor da maior cédula,
+ *      disponibilizar a próxima.
+ *
+ *
+ * 6.   Se o valor a ser solicitado for maior que o saldo total do caixa,
+ *      enviar a mensagem "EXCEDEU O LIMITE DO CAIXA".
+ *
+ *
+ * 7.   Solicitar até 100 retiradas ou até não haver mais notas.
+ *
+ *
+ * 8.   No momento da solicitação do valor, coletar também o código
+ *      do banco que o cliente tem conta, segundo:
+ *
+ *      Código: [       1       ][      2      ][      3     ][      4      ]
+ *      Banco:  [Banco do Brasil][  Santander  ][    Itaú    ][    Caixa    ]
+ *
+ *
+ * 9.   No final, exibir a estatística, separada por bancos, com:
+ *
+ *      9.1 O maior e o menor valor sacado;
+ *      
+ *      9.2 A média dos saques;
+ *
+ *      9.3 Valor total dos saques;
+ *
+ *      9.4 Valor das sobras dos caixas.
+ */
+
+/**
+ *
+ * @author Bruno Bega Harnik
+ * Professor: Ricardo Satoshi
+ * 
+ */
+
+import javax.swing.JOptionPane;
+
+public class Lt01_CaixaEletronico {
+    
+    public static void main(String[] args){
+        
+        
+        
+    }
+    
+}
