9cd5d00b3492f15ad09de49616d13ec90e19f1ef
Bruno Harnik <brunoharnik@gmail.com>
2019-05-27 02:35:09 -0300

    Ex. Aluno

--- 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/SistemaAluno/src/sistemaaluno/Aluno.java
+++ 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/SistemaAluno/src/sistemaaluno/Aluno.java
@@ -6,25 +6,82 @@
  */
 package sistemaaluno;
 
+import java.io.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author bruno
  */
 public class Aluno {
 
-    public String nome;
-    public String sobrenome;
-    private double pontos;
+    private String nome;
+    private String sobrenome;
+    private float pontos;
+    
+    public Aluno[] gravarAluno(Aluno[] aluno, int qtd) throws IOException {
+
+        int i;
+
+        String nome_do_arquivo = "Registro_de_Alunos.txt";
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter( nome_do_arquivo, true ));
+        
+        //Inicio um objeto Aluno em cada posição de meu array aluno[]
+        for (i = 0 ; i < qtd ; i++)	
+             aluno[i] = new Aluno();
+
+        for (i = 0 ; i < qtd ; i++)  {	
+
+            //Define NOME e escreve no TXT
+            aluno[i].setNome(JOptionPane.showInputDialog("Digite o primeiro nome do aluno:"));	
+            writer.write("Aluno(a): " + aluno[i].getNome() + " ");
+
+            //Define SOBRENOME e escreve no TXT
+            aluno[i].setSobrenome(JOptionPane.showInputDialog("Digite o sobrenome de " + aluno[i].getNome() + ":"));	
+            writer.write(aluno[i].getSobrenome());
+            writer.newLine();
+
+            //Define PONTOS e escreve no TXT
+            aluno[i].setPontos(Float.parseFloat(JOptionPane.showInputDialog("Pontos de graduação de " + aluno[i].getNome() + " " + aluno[i].getSobrenome() + ":")));	
+            writer.write("Pontos: " + Float.toString(aluno[i].getPontos()) + "\n-------------------------------");
+            writer.newLine();
+        }
+
+        //Mensagem de sucesso e fechamento do BUFFERED WRITER
+        JOptionPane.showMessageDialog(null, "Registro realizado com sucesso!");
+        writer.close();
+        return aluno;
+    }
+    
+    public void lerAluno() throws IOException {
+        
+        int i;
+        
+        String nome_do_arquivo = "Registro_de_Alunos.txt";
+        
+        BufferedReader br = new BufferedReader(new FileReader( nome_do_arquivo));
+        String s;
+        String texto = "";
+             
+            //ENQUANTO EXISTE DADOS CONTINUA IMPRIMINDO
+            while ((s = br.readLine()) != null) {
+                texto += s + "\n";
+            }
+            
+        JOptionPane.showMessageDialog(null, "Registros gravados na lista de alunos:\n\n-------------------------------\n" + texto);
+        br.close();
+    }
 
     // Métodos construtores: 1º - Recebendo em array, 2º - Declaração de cada variável	 
     Aluno() {
         this("", "", 0);
     }
 
-    Aluno(String nomealuno, String sobrenomealuno, double pontosaluno){
-          nome = nomealuno;
-          sobrenome = sobrenomealuno;
-          pontos = pontosaluno;
+    Aluno(String nomealuno, String sobrenomealuno, float pontosaluno){
+          this.nome = nomealuno;
+          this.sobrenome = sobrenomealuno;
+          this.pontos = pontosaluno;
     }
     
     //Métodos get & set de encapsulamento
@@ -44,13 +101,12 @@ public class Aluno {
         this.sobrenome = sobrenome;
     }
 
-    public double getPontos() {
+    public float getPontos() {
         return pontos;
     }
 
-    public void setPontos(double pontos) {
+    public void setPontos(float pontos) {
         this.pontos = pontos;
     }
-    
     
 }
--- 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/SistemaAluno/src/sistemaaluno/SistemaAluno.java
+++ 2SEM/ILP010 - Linguagem de Programação/Bruno Harnik/SistemaAluno/src/sistemaaluno/SistemaAluno.java
@@ -10,21 +10,22 @@ package sistemaaluno;
  *
  * @author Bruno
  */
-import java.io.*;
 import javax.swing.*;
+import java.io.*;
 
 class ManipulaDados {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         int opcao = 0;
         
         JOptionPane.showMessageDialog(null, "Bem vindo(a) ao sistema de aluno.");
         
-        while(opcao < 1 || opcao > 2){
+        while((opcao != 9)){
         
-        opcao = Integer.parseInt(JOptionPane.showInputDialog("1: Registrar aluno e pontos\n\n"
+        opcao = Integer.parseInt(JOptionPane.showInputDialog("1: Registrar aluno e pontos\n"
                                                            + "2: Verificar lista de alunos e pontos.\n\n"
+                                                           + "9: Encerrar programa.\n\n"
                                                            + "Digite a opção desejada:"));
         
             switch(opcao){
@@ -35,30 +36,19 @@ class ManipulaDados {
                             //Inicio um objeto array que recebe a Classe Aluno
                             Aluno[] aluno = new Aluno[qtd];
                             
-                            //Variáveis de controle
-                            int i, ptos;
-                            
-                            //For: cada posição do array é um novo objeto de Classe Aluno
-                            for (i = 0; i < qtd; i++) {
-                                aluno[i] = new Aluno();
-                            }
-                            
-                            //For: entrada de dados em cada posição
-                            for (i = 0; i < qtd; i++) {
-                                aluno[i].setNome(JOptionPane.showInputDialog("Digite o primeiro nome do aluno:"));
-                                aluno[i].setSobrenome(JOptionPane.showInputDialog("Digite o sobrenome do aluno:\n" + aluno[i].getNome() + "..."));
-                                aluno[i].setPontos(Float.parseFloat(JOptionPane.showInputDialog("Pontos de graduação de " + aluno[i].getNome() + " " + aluno[i].getSobrenome() + ":")));
-                            }
-                            
-                            //For: print do sistema
-                            for (i = 0; i < qtd; i++) {
-                                System.out.println("\nAluno: " + aluno[i].getNome() + " " + aluno[i].getSobrenome() + "\n Pontos: " + aluno[i].getPontos());
-                            }
-                            System.exit(0);
+                            //Inicio um objeto e executo o método de gravação
+                            Aluno g = new Aluno();
+                            g.gravarAluno(aluno, qtd);
                             
                             break;
                 
-                case 2:     
+                case 2:     //Inicio um objeto para executar o método de leitura
+                            Aluno l = new Aluno();
+                            l.lerAluno();
+                            
+                            break;
+                
+                case 9:     JOptionPane.showMessageDialog(null, "Obrigado por testar o Sistema de Aluno.\nFim do programa.");
                             break;
                 
                 default:    JOptionPane.showMessageDialog(null, "Opção inválida.");
