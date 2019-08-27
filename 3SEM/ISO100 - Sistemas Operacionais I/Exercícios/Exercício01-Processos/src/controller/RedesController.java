6fa8457ec5595ee3221e3f2ee74e69f7d599d949
Bruno Harnik <brunoharnik@gmail.com>
2019-08-27 17:29:40 -0300

    Chupando manga

--- 3SEM/ISO100 - Sistemas Operacionais I/Exercícios/Exercício01-Processos/src/controller/RedesController.java
+++ 3SEM/ISO100 - Sistemas Operacionais I/Exercícios/Exercício01-Processos/src/controller/RedesController.java
@@ -1,9 +1,30 @@
 package controller;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+import static java.lang.System.out;
 
-public class RedesController {
+public class RedesController{
 	
+	public static void redes() throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
+    }
+	
+	
+	static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        out.printf("Nome: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+
+        out.printf("\n");
+     }
 	
 	
 	public String os(){
