5ec31d82a35de130f8ed6e66663fb3115dece0b3
Bruno Harnik <brunoharnik@gmail.com>
2019-08-30 01:28:29 -0300

    Python

---
+++ Python com Satoshi/CalcLado.py
@@ -0,0 +1,8 @@
+class CalcLado:
+    a = 2
+    b = 1
+    c = a + b
+    print(c)
+
+    def CalcSalario(salario, tx):
+        salario += salario * tx
---
+++ Python com Satoshi/main.py
@@ -0,0 +1,13 @@
+import CalcLado
+
+salario = input('Digite o salario do funcionario')
+tx = input('Digite a taxa de aumento em %')
+
+
+
+    def CalcSalario(salario, tx):
+        salario += salario * tx
+
+calcSalario(salario, tx)
+
+
