f505694ba740b8cc50fd578626962f767f94dc47
Fernanda Reis <fpreis98@gmail.com>
2019-08-31 02:36:23 -0300

    casos de uso

--- Python com Satoshi/CalcEquacao_2_Grau.py
+++ Python com Satoshi/CalcEquacao_2_Grau.py
@@ -4,12 +4,20 @@ a = input("Digite o valor de a: ")
 b = input("Digite o valor de b: ")
 c = input("Digite o valor de c: ")
 
-def bhaskara (a,b,c):
+def bhaskara (self, a,b,c):
+    self.a = a
+    self.b = b
+    self.c = c
+
     delta = math.sqrt(b) - (4 * a * c)
 
     if delta < 0:
         print("A equação não possui raízes reais")
     elif delta == 0:
-
+        raiz = -b / (2 * a)
+        print(raiz)
     else:
-        bhask = -b
+        raiz = (-1 * b + math.sqrt(delta)) / (2*a)
+        print(raiz)
+        raiz = (-1 * b - math.sqrt(delta)) / (2*a)
+        print(raiz)
