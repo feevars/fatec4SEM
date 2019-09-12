ac024921b51dcf4ffcf2cb0bde7e47a7637b5e90
Bruno Harnik <brunoharnik@gmail.com>
2019-09-12 10:48:04 -0300

    Note

---
+++ Python com Satoshi/Bruno/lote_um.py
@@ -0,0 +1,9 @@
+def ex01():
+    print("Cálculo de área de retângulo:")
+    a = float(input("Digite o tamanho do primeiro lado: "))
+    b = float(input("Digite o tamanho do segundo lado: "))
+    area = float(a * b)
+    print("A área do retângulo é de ", area, "m²")
+def ex02():
+    print("Receba o salário de um funcionário e mostre o novo salário com reajuste de 15%.")
+    a = float(input("Digite o valor do salário: R$"))
--- Python com Satoshi/CalcSalario.py
+++
@@ -1,7 +0,0 @@
-
-
-salario = input('Digite o salario do funcionario')
-tx = input("Digite a taxa de aumento em %")
-
-def CalcSalario(salario, tx):
-    salario += (salario * tx)
