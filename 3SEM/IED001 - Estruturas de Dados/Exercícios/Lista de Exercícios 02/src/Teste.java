7fed3a9fb579b0f0969b5233757a68919134f7ee
Bruno Harnik <brunoharnik@gmail.com>
2019-09-21 17:29:50 -0300

    fsad

---
+++ 3SEM/IED001 - Estruturas de Dados/Exercícios/Lista de Exercícios 02/src/Teste.java
@@ -0,0 +1,14 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		double a = 10.4;
+		double b = 10.56;
+		
+		if (a < b) {
+			System.out.println(a + " é menor que " + b);
+		}
+	}
+
+}
