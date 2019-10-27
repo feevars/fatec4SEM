eeb1f13d5147f1608dcb0eb292b44ac4e752dd81
Bruno Harnik <brunoharnik@gmail.com>
2019-10-26 21:16:53 -0300

    Novos ícones na view

--- 3SEM/IED001 - Estruturas de Dados/Trabalho/Lista de Convidados/src/view/BarraDeFerramentas.java
+++ 3SEM/IED001 - Estruturas de Dados/Trabalho/Lista de Convidados/src/view/BarraDeFerramentas.java
@@ -6,9 +6,12 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class BarraDeFerramentas extends JPanel {
 	
@@ -20,11 +23,18 @@ public class BarraDeFerramentas extends JPanel {
 	private JTextField campoBuscar;
 	private JButton btnBuscar;
 	
+	private Icon iconeNova = new ImageIcon("src/assets/icone_nova.png");
+	private Icon iconeAbrir = new ImageIcon("src/assets/icone_abrir.png");
+	private Icon iconeSalvar = new ImageIcon("src/assets/icone_salvar.png");
+	private Icon iconeOrdenar = new ImageIcon("src/assets/icone_ordenar.png");
+	private Icon iconeEstatisticas = new ImageIcon("src/assets/icone_estatisticas.png");
+	private Icon iconeBuscar = new ImageIcon("src/assets/icone_buscar.png");
+	
 	public BarraDeFerramentas() {
-		btnNovaLista = new JButton("Nova...");
-		btnAbrirLista = new JButton("Abrir...");
-		btnSalvarLista = new JButton("Salvar...");
-		btnOrdenarPor = new JButton("Ordenar por...");
+		btnNovaLista = new JButton("Nova");
+		btnAbrirLista = new JButton("Abrir");
+		btnSalvarLista = new JButton("Salvar");
+		btnOrdenarPor = new JButton("Ordenar por");
 		btnEstatisticas = new JButton("Estatisticas");
 		campoBuscar = new JTextField();
 		btnBuscar = new JButton("Buscar");
@@ -40,24 +50,28 @@ public class BarraDeFerramentas extends JPanel {
 		estiloBotao(btnBuscar);
 		
 		add(btnNovaLista);
+		btnNovaLista.setIcon(iconeNova);
 		add(btnAbrirLista);
+		btnAbrirLista.setIcon(iconeAbrir);
 		add(btnSalvarLista);
+		btnSalvarLista.setIcon(iconeSalvar);
 		add(btnOrdenarPor);
+		btnOrdenarPor.setIcon(iconeOrdenar);
 		add(btnEstatisticas);
+		btnEstatisticas.setIcon(iconeEstatisticas);
 		
 		add(campoBuscar);
 		add(btnBuscar);
+		btnBuscar.setIcon(iconeBuscar);
 	}
 	
 	public JButton estiloBotao(JButton b) {
 		
 		b.setBackground(new Color(46,41,79));
-		b.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+		b.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
 		b.setForeground(Color.WHITE);
-		b.setMinimumSize(new Dimension(100,40));
-		b.setPreferredSize(new Dimension(140,50));
-		b.setMaximumSize(new Dimension(200,50));
-		
+		b.setMinimumSize(new Dimension(100,60));
+		b.setMaximumSize(new Dimension(200,60));
 		return b;
 	}
 
