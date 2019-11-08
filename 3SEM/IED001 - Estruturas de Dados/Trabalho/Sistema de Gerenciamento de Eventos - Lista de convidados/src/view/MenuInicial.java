62840427fc8211e10c175ebd4686a1973f3f575c
Bruno Harnik <brunoharnik@gmail.com>
2019-11-08 17:21:53 -0300

    Tela de abertura

--- 3SEM/IED001 - Estruturas de Dados/Trabalho/Sistema de Gerenciamento de Eventos - Lista de convidados/src/view/MenuInicial.java
+++ 3SEM/IED001 - Estruturas de Dados/Trabalho/Sistema de Gerenciamento de Eventos - Lista de convidados/src/view/MenuInicial.java
@@ -12,12 +12,16 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Dimension;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.GridLayout;
 
 public class MenuInicial extends JFrame {
 
 	private static final long serialVersionUID = -626919165931577521L;
 	
 	private JPanel contentPane;
+	private static final Color AZUL_ESCURO = new Color(46, 41, 78);
+	private static final Color CINZA = new Color(75, 82, 103);
 
 	public MenuInicial() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,23 +29,31 @@ public class MenuInicial extends JFrame {
 		setResizable(false);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(AZUL_ESCURO);
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel painelBotoes = new JPanel();
+		painelBotoes.setPreferredSize(new Dimension(300, 50));
+		painelBotoes.setBackground(AZUL_ESCURO);
 		contentPane.add(painelBotoes, BorderLayout.SOUTH);
-		painelBotoes.setLayout(new BoxLayout(painelBotoes, BoxLayout.X_AXIS));
+		painelBotoes.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JButton btnNovaLista = new JButton("Nova Lista");
+		JButton btnNovaLista = new JButton("Novo Evento");
+		btnNovaLista.setForeground(Color.WHITE);
+		btnNovaLista.setIcon(new ImageIcon(MenuInicial.class.getResource("/assets/icone_novo.png")));
+		btnNovaLista.setBackground(CINZA);
 		painelBotoes.add(btnNovaLista);
 		
 		
-		JButton btnAbrirListaExistente = new JButton("Abrir Lista Existente");
+		JButton btnAbrirListaExistente = new JButton("Abrir Evento");
+		btnAbrirListaExistente.setForeground(Color.WHITE);
+		btnAbrirListaExistente.setIcon(new ImageIcon(MenuInicial.class.getResource("/assets/icone_abrir.png")));
+		btnAbrirListaExistente.setBackground(CINZA);
 		painelBotoes.add(btnAbrirListaExistente);
 		
-		JLabel lblImagemPrincipal = new JLabel("");
-		lblImagemPrincipal.setIcon(new ImageIcon("/Users/bruno/Desktop/Fatec/3SEM/IED001 - Estruturas de Dados/Trabalho/Sistema de Gerenciamento de Eventos - Lista de convidados/bin/img/cu.jpg"));
+		JLabel lblImagemPrincipal = new JLabel();
+		lblImagemPrincipal.setIcon(new ImageIcon(MenuInicial.class.getResource("/assets/imagem-abertura.png")));
 		lblImagemPrincipal.setPreferredSize(new Dimension(300, 230));
 		lblImagemPrincipal.setMinimumSize(new Dimension(300, 230));
 		contentPane.add(lblImagemPrincipal, BorderLayout.CENTER);
