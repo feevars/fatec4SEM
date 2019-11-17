9197254f1838cf2e15e44bc5b2c9f6e3228eda0f
Bruno Harnik <brunoharnik@gmail.com>
2019-11-16 22:17:14 -0300

    ex 5, lista 8, ED

--- 3SEM/IED001 - Estruturas de Dados/Exercícios/Lista 08/src/Lista08.java
+++ 3SEM/IED001 - Estruturas de Dados/Exercícios/Lista 08/src/Lista08.java
@@ -1,21 +1,26 @@
 import controller.HeapSort;
 
 public class Lista08{
-
+	
+	/**
+	 * Método principal da Lista 08 - Estruturas de Dados
+	 * FATEC Zona Leste - Professor Fábio Pereira da Silva
+	 * 
+	 * Desenvolvedor: Bruno Bega Harnik
+	 */
 	public static void main(String[] args) {
-		
-		
 		
 		int [] vetor = {87, 6, 3, 5, 1, 9, 42, 43, 57, 71, 23,21, 98};
 		
+		System.out.println("Vetor do exercício:");
 		System.out.println(HeapSort.printaVetor(vetor));
 		
 		HeapSort.sortMaximo(vetor);
+		System.out.println("\n\nHeapSort (Máximo):");
 		System.out.println(HeapSort.printaVetor(vetor));
 		
 		HeapSort.sortMinimo(vetor);
+		System.out.println("\n\nHeapSort (Mínimo):");
 		System.out.println(HeapSort.printaVetor(vetor));
-
 	}
-
 }
--- 3SEM/IED001 - Estruturas de Dados/Exercícios/Lista 08/src/controller/HeapSort.java
+++ 3SEM/IED001 - Estruturas de Dados/Exercícios/Lista 08/src/controller/HeapSort.java
@@ -2,28 +2,12 @@ package controller;
 
 public interface HeapSort {
 
-	public static void sortMaximo(int vetor[]) {
 
-		int tamanho = vetor.length;
-
-		// Construir o "heap" (lote) – rearranjando o vetor
-		for (int i = tamanho / 2 - 1; i >= 0; i--) {
-			heapMaximo(vetor, tamanho, i);
-		}
-
-		// Um a um, extrai um elemento do "heap"
-		for (int i = tamanho - 1; i >= 0; i--) {
-
-			// Move a atual raiz para o final
-			int aux = vetor[0];
-			vetor[0] = vetor[i];
-			vetor[i] = aux;
-
-			// Chama o Heap Máximo no "heap" restante
-			heapMaximo(vetor, i, 0);
-		}
-	}
-
+	/**
+	 * Método Sort Mínimo: realiza o HeapSort Mínimo
+	 * 
+	 * @param vetor[] = vetor de inteiros
+	 */
 	public static void sortMinimo(int vetor[]) {
 
 		int tamanho = vetor.length;
@@ -36,7 +20,7 @@ public interface HeapSort {
 		// Um a um, extrai um elemento do "heap"
 		for (int i = tamanho - 1; i >= 0; i--) {
 
-			// Move a atual raiz para o começo
+			// Move a atual raiz para o final
 			int aux = vetor[0];
 			vetor[0] = vetor[i];
 			vetor[i] = aux;
@@ -45,32 +29,41 @@ public interface HeapSort {
 			heapMinimo(vetor, i, 0);
 		}
 	}
+	
+	/**
+	 * Método Sort Máximo: realiza o HeapSort Máximo
+	 * 
+	 * @param vetor[] = vetor de inteiros
+	 */
+	public static void sortMaximo(int vetor[]) {
 
-	public static void heapMinimo(int[] vetor, int tamanho, int i) {
+		int tamanho = vetor.length;
 
-		int menor = i; // Inicia o menor como raiz
-		int esquerda = 2 * i + 1;
-		int direita = 2 * i + 2;
+		// Construir o "heap" (lote) – rearranjando o vetor
+		for (int i = tamanho / 2 - 1; i >= 0; i--) {
+			heapMaximo(vetor, tamanho, i);
+		}
 
-		// Se o filho da esquerda é menor que a raiz...
-		if (esquerda < tamanho && vetor[esquerda] < vetor[menor])
-			menor = esquerda;
+		// Um a um, extrai um elemento do "heap"
+		for (int i = tamanho - 1; i >= 0; i--) {
 
-		// Se o filho da direita é menor que o menor...
-		if (direita < tamanho && vetor[direita] < vetor[menor])
-			menor = direita;
+			// Move a atual raiz para o começo
+			int aux = vetor[0];
+			vetor[0] = vetor[i];
+			vetor[i] = aux;
 
-		// Se o maior não é a raiz
-		if (menor != i) {
-			int troca = vetor[i];
-			vetor[i] = vetor[menor];
-			vetor[menor] = troca;
-
-			// Chamada recursiva para os próximos níveis da árvore
-			heapMinimo(vetor, tamanho, menor);
+			// Chama o Heap Máximo no "heap" restante
+			heapMaximo(vetor, i, 0);
 		}
 	}
-
+	
+	/**
+	 * Método Heap Máximo - Realiza a inserção no "heap" a partir do máximo
+	 * 
+	 * @param vetor[] - vetor de inteiros
+	 * @param tamanho - tamanho do vetor (inteiro)
+	 * @param i - índice, usado para o maior valor
+	 */
 	public static void heapMaximo(int[] vetor, int tamanho, int i) {
 
 		int maior = i; // Inicia o maior como raiz
@@ -78,11 +71,11 @@ public interface HeapSort {
 		int direita = 2 * i + 2;
 
 		// Se o filho da esquerda é maior que a raiz...
-		if (esquerda < tamanho && vetor[esquerda] > vetor[maior])
+		if (esquerda < tamanho && vetor[esquerda] < vetor[maior])
 			maior = esquerda;
 
-		// Se o filho da direita é maior que o maior...
-		if (direita < tamanho && vetor[direita] > vetor[maior])
+		// Se o filho da direita é maior que o menor...
+		if (direita < tamanho && vetor[direita] < vetor[maior])
 			maior = direita;
 
 		// Se o maior não é a raiz
@@ -95,7 +88,45 @@ public interface HeapSort {
 			heapMaximo(vetor, tamanho, maior);
 		}
 	}
+	
+	/**
+	 * Método Heap Mínimo - Realiza a inserção no "heap" a partir do mínimo
+	 * 
+	 * @param vetor[] - vetor de inteiros
+	 * @param tamanho - tamanho do vetor (inteiro)
+	 * @param i - índice, usado para o menor valor
+	 */
+	public static void heapMinimo(int[] vetor, int tamanho, int i) {
 
+		int menor = i; // Inicia o menor como raiz
+		int esquerda = 2 * i + 1;
+		int direita = 2 * i + 2;
+
+		// Se o filho da esquerda é menor que a raiz...
+		if (esquerda < tamanho && vetor[esquerda] > vetor[menor])
+			menor = esquerda;
+
+		// Se o filho da direita é menor que o maior...
+		if (direita < tamanho && vetor[direita] > vetor[menor])
+			menor = direita;
+
+		// Se o menor não é a raiz
+		if (menor != i) {
+			int troca = vetor[i];
+			vetor[i] = vetor[menor];
+			vetor[menor] = troca;
+
+			// Chamada recursiva para os próximos níveis da árvore
+			heapMinimo(vetor, tamanho, menor);
+		}
+	}
+	
+	/**
+	 * Método que retorna uma String com o vetor solicitado, um "toString" da vida...
+	 * 
+	 * @param vetor
+	 * @return
+	 */
 	public static String printaVetor(int vetor[]) {
 
 		String vetorTexto = "[";
@@ -108,7 +139,6 @@ public interface HeapSort {
 				vetorTexto += "]";
 			}
 		}
-
 		return vetorTexto;
 	}
 }
