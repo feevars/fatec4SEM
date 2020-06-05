3b72c1c6b7489a30a3aea489f84cc44534288c3c
feevars <feevars@gmail.com>
2020-06-05 20:15:16 -0300

    linux, apresentacoes e mais coisinhas

---
+++ ILP007-Programação Orientada a Objetos/Algoritmos feitos em casa/Fernanda/Aula09/src/edu/crudaluno/entities/Aluno.java
@@ -0,0 +1,5 @@
+package edu.crudaluno.entities;
+
+public class Aluno {
+
+}
---
+++ ILP007-Programação Orientada a Objetos/Algoritmos feitos em casa/Fernanda/Aula09/src/edu/crudaluno/view/AlunoBoundary.java
@@ -0,0 +1,18 @@
+package edu.crudaluno.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AlunoBoundary extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(AlunoBoundary.class, args);
+	}
+	
+}
