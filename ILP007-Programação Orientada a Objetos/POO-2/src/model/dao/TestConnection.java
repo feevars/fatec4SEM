90c8ebce27c69ed16bd672fd3b3dcbd60afbcfb2
Luiz Fernando <lfernando488@gmail.com>
2020-06-26 03:28:30 -0300

    Database Factory

---
+++ ILP007-Programação Orientada a Objetos/POO-2/src/model/dao/TestConnection.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		DaoFactory conFactory =  new DaoFactory();
+		
+		conFactory.getConnection();
+		
+		
+		
+	}
+
+}
--- ILP007-Programação Orientada a Objetos/POO-2/src/model/dao/implementation/CursoDaoImpl.java
+++ ILP007-Programação Orientada a Objetos/POO-2/src/model/dao/implementation/CursoDaoImpl.java
@@ -1,5 +1,50 @@
 package model.dao.implementation;
 
-public class CursoDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.dao.CursoDao;
+import model.dao.DaoFactory;
+import model.entities.Curso;
+
+public class CursoDaoImpl implements CursoDao {
+	
+	DaoFactory dbFactory = new DaoFactory();
+	Connection con = dbFactory.getConnection();
+	
+	@Override
+	public void adicionarCurso(Curso c) {
+		try {
+			String sql = "INSERT INTO Curso (id, nome, descricao, totalHoras, dataCriacao) "
+					+ "VALUES  (0, ?, ?, ?, ?)";
+			PreparedStatement stm = con.prepareStatement(sql);
+			stm.setInt(1, c.getId());
+			stm.setString(2, c.getNome());
+			stm.executeUpdate();
+			con.close();
+		}catch ( SQLException e ){
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void excluirCurso(Curso c) {
+		
+		
+	}
+
+	@Override
+	public void editarCurso(Curso c) {
+		
+		
+	}
+
+	@Override
+	public List<Curso> pesquisarPorNome(String nome) {
+		
+		return null;
+	}
 
 }
